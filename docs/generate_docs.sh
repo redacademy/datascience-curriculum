8863aa6fee43a050d8f2ac8e842b860db80cf074
Jill Cates <cates.jill@gmail.com>
2020-02-28 13:33:26 -0500

    Update generate doc script, add favicon

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -1,17 +1,23 @@
 mkdir source/notebooks/
-mkdir source/markdown/
 
 echo "Copying files over..."
 cp -r ../part1/*ipynb source/notebooks/
+cp ../part1/README.md source/notebooks/part1_readme.md
+
 cp -r ../part2/*ipynb source/notebooks/
+cp ../part2/README.md source/notebooks/part2_readme.md
+
 cp -r ../part3/*ipynb source/notebooks/
+cp ../part3/README.md source/notebooks/part3_readme.md
+
 cp -r ../part4/*ipynb source/notebooks/
+cp ../part4/README.md source/notebooks/part4_readme.md
 
 echo "Generating docs..."
 make html
 
-# echo "Cleaning up..."
-# rm -rf source/notebooks/
+echo "Cleaning up..."
+rm -rf source/notebooks/
 
 
 #! echo "Copying docs to doc folder..."
